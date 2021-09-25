689ed959554904c21cb2d7dc6dd8fbe899cd4398
emilyann <easheridan@gmail.com>
2021-09-25 10:01:48 -0700

    js updates after dev q and a

--- js/script.js
+++ js/script.js
@@ -3,12 +3,13 @@ const guessButton = document.querySelector(".guess");
 const letterInput = document.querySelector(".letter");
 const wordInProgress = document.querySelector(".word-in-progress");
 const remaining = document.querySelector(".remaining");
-const remainingGuesses = document.querySelector(".remaining span");
+const remainingSpan = document.querySelector(".remaining span");
 const guessReply = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
 
 const word = "magnolia";
 const guessedLetters = [];
+const remainingGuesses = 8;
 
 // Display our symbols as placeholders for the chosen word's letters
 const placeholder = function (word) {
@@ -64,6 +65,7 @@ const makeGuess = function (guess) {
     } else {
         guessedLetters.push(guess);
         console.log(guessedLetters);
+        countGuesses(guess);
         updateLetters();
         updateWordInProgress(guessedLetters);
     }
@@ -99,6 +101,24 @@ const updateWordInProgress = function (guessedLetters) {
     checkWinner();
 };
 
+const countGuesses = function (guess) {
+    const upperWord = word.toUpperCase();
+    if (!upperWord.includes(guess)) {
+        guessReply.innerText = `Oops! The word doesn't contain ${guess}!`;
+        remainingGuesses -= 1;
+    } else {
+        guessReply.innerText = `Way to go! The word contains the letter ${guess}!`;
+    }
+
+    if (remainingGuesses === 0) {
+        guessReply.innerHTML = `Aw shucks, no more guesses! The word was <span class="highlight">${word}</span>.`
+    } else if (remainingGuesses === 1) {
+        remainingSpan.innerText = `${remainingGuesses} guess`;
+    } else {
+        remainingSpan.innerText = `${remainingGuesses} guesses`;
+    }
+
+};
 
 const checkWinner = function () {
     if (word.toUpperCase() === wordInProgress.innerText) {
